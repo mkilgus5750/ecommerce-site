416fe535c7f780d86c93630d3031f0235b4ac21f
John L. Armstrong IV <jlarmst@g.clemson.edu>
2020-06-24 17:40:03 -0400

    feat: configure tailwindcss

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,13 @@
 import React from "react"
-import './Footer.css'
+import "./Footer.css"
 
 export default function Footer() {
-    return(
-        <footer className="footer__container">
-            <div className="footer">
-                <p className="footer__copyright">Created by Mari Kilgus © 2020</p>
-                <p>Contact:
-                marikilgus@gmail.com
-                </p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="bg-blue-500 footer__container">
+      <div className="footer">
+        <p className="footer__copyright">Created by Mari Kilgus © 2020</p>
+        <p>Contact: marikilgus@gmail.com</p>
+      </div>
+    </footer>
+  )
 }
